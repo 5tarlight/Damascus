179580652d9fddb7efa62dc7a9b441b926544ac9
5tarlight <developerstarlight@gmail.com>
2021-05-31 00:20:53 +0900

    bootstrap input search

--- frontend/src/Components/Header/HeaderSearch/HeaderSearch.tsx
+++ frontend/src/Components/Header/HeaderSearch/HeaderSearch.tsx
@@ -1,14 +1,22 @@
 import { FC } from 'react'
 import styles from './HeaderSearch.scss'
 import classNames from 'classnames/bind'
+import { Button, FormControl, InputGroup } from 'react-bootstrap'
 
 const cx = classNames.bind(styles)
 
 const HeaderSearch: FC<{}> = () => {
   return (
-    <form>
-      <input className={cx('search-input')} />
-    </form>
+    <InputGroup className={cx('mb-3', 'search')}>
+      <FormControl
+        placeholder="Recipient's username"
+        aria-label="Recipient's username"
+        aria-describedby="basic-addon2"
+      />
+      <InputGroup.Append>
+        <Button variant="outline-secondary">Button</Button>
+      </InputGroup.Append>
+    </InputGroup>
   )
 }
 
