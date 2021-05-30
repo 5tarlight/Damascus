80888bd62eadedfee7fa5d518a2759b87a30267c
5tarlight <developerstarlight@gmail.com>
2021-05-30 23:50:55 +0900

    fluidy component:

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { Component } from 'react'
 import Logo from './Logo/Logo'
 import styles from './Header.scss'
 import classNames from 'classnames/bind'
@@ -7,22 +7,48 @@ import HeaderAuthItem from './HeaderAuthItem/HeaderAuthItem'
 
 const cx = classNames.bind(styles)
 
-const Header: FC<{}> = () => (
-  <div className={cx('header')}>
-    <Logo />
-    <div>
-      <ul className={cx('item-list')}>
-        <li><HeaderItem text='커미션' to='/commisions' /></li>
-        <li><HeaderItem text='커미션' to='/commisions' /></li>
-      </ul>
-    </div>
-    <nav>
-      <ul className={cx('item-list')}>
-        <li><HeaderAuthItem text='로그인' to='/auth/login' /></li>
-        <li><HeaderAuthItem text='회원가입' to='/auth/signup' /></li>
-      </ul>
-    </nav>
-  </div>
-)
+interface State {
+  width: number
+}
+
+class Header extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      width: window.innerWidth
+    }
+  }
+
+  render() {
+    const menu = this.state.width >= 748 ?
+      (
+        <>
+          <li><HeaderItem text='커미션' to='/commisions' /></li>
+          <li><HeaderItem text='커미션' to='/commisions' /></li>
+        </>
+      ): (
+        <div>
+
+        </div>
+      )
+
+    return (
+      <div className={cx('header')}>
+        <Logo />
+        <div>
+          <ul className={cx('item-list')}>
+            {menu}
+          </ul>
+        </div>
+        <nav>
+          <ul className={cx('item-list')}>
+            <li><HeaderAuthItem text='로그인' to='/auth/login' /></li>
+            <li><HeaderAuthItem text='회원가입' to='/auth/signup' /></li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+}
 
 export default Header
