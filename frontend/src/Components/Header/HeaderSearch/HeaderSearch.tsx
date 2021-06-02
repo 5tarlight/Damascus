091eea70b4720301a57e8bc34d136972f576d2b4
5tarlight <developerstarlight@gmail.com>
2021-06-02 20:28:28 +0900

    search input state

--- frontend/src/Components/Header/HeaderSearch/HeaderSearch.tsx
+++ frontend/src/Components/Header/HeaderSearch/HeaderSearch.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { ChangeEvent, Component } from 'react'
 import styles from './HeaderSearch.scss'
 import classNames from 'classnames/bind'
 import { InputGroup } from 'react-bootstrap'
@@ -7,13 +7,36 @@ import SearchBtn from './SearchBtn/SearchBtn'
 
 const cx = classNames.bind(styles)
 
-const HeaderSearch: FC<{}> = () => {
-  return (
-    <InputGroup className={cx('mb-3', 'search')}>
-      <SearchInput />
-      <SearchBtn />
-    </InputGroup>
-  )
+interface State {
+  value: string
+}
+
+class HeaderSearch extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props)
+
+    this.state = {
+      value: ''
+    }
+  }
+
+  render() {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      this.setState({
+        value: e.target.value
+      })
+    }
+
+    return (
+      <InputGroup className={cx('mb-3', 'search')}>
+        <SearchInput
+          text={this.state.value}
+          handleChange={handleChange}
+        />
+        <SearchBtn />
+      </InputGroup>
+    )
+  }
 }
 
 export default HeaderSearch
--- frontend/src/Components/Header/HeaderSearch/SearchInput/SearchInput.tsx
+++ frontend/src/Components/Header/HeaderSearch/SearchInput/SearchInput.tsx
@@ -1,19 +1,34 @@
-import { FC } from 'react'
+import { ChangeEvent, Component } from 'react'
 import styles from './SearchInput.scss'
 import classNames from 'classnames/bind'
 import { FormControl } from 'react-bootstrap'
 
 const cx = classNames.bind(styles)
 
-const SearchInput: FC<{}> = () => {
-  return (
-    <FormControl
+interface Props {
+  handleChange(e: ChangeEvent<HTMLInputElement>): void
+  text: string
+}
+
+class SearchInput extends Component<Props, {}> {
+  shouldComponentUpdate(nextProps: Props, nextState: {}): boolean {
+    return this.props.text !== nextProps.text
+  }
+
+  render() {
+    const { handleChange, text } = this.props
+
+    return (
+      <FormControl
         className={cx('search-input')}
         placeholder="카테고리"
         aria-label="Category"
         aria-describedby="basic-addon2"
+        onChange={handleChange}
+        value={text}
       />
-  )
+    )
+  }
 }
 
 export default SearchInput
