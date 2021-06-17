3b2ce562387e59f4894ef6958e8c3ae8cd1c4a82
5tarlight <developerstarlight@gmail.com>
2021-06-17 21:34:11 +0900

    failure message ref

--- frontend/src/Page/SignUp.tsx
+++ frontend/src/Page/SignUp.tsx
@@ -1,6 +1,7 @@
-import { Dispatch, FC, memo, SetStateAction, useState } from 'react'
+import { createRef, Dispatch, FC, memo, SetStateAction, useState } from 'react'
 import AuthBox from '../Components/Auth/AuthBox/AuthBox'
 import AuthInput from '../Components/Auth/AuthInput/AuthInput'
+import AuthMessage from '../Components/Auth/AuthInput/AuthMesssage/AuthMessage'
 import AuthTitle from '../Components/Auth/AuthTitle/AuthTitle'
 
 const SignUp: FC<{}> = () => {
@@ -8,6 +9,11 @@ const SignUp: FC<{}> = () => {
   const [pw, setPw] = useState('')
   const [pwCon, setPwCon] = useState('')
   const [username, SetUsername] = useState('')
+
+  const emailRef = createRef<HTMLDivElement>()
+  const pwRef = createRef<HTMLDivElement>()
+  const pwConRef = createRef<HTMLDivElement>()
+  const usernameRef = createRef<HTMLDivElement>()
 
   const handleChnage = (dispatch: Dispatch<SetStateAction<string>>) => {
     return (str: string) => {
@@ -24,11 +30,16 @@ const SignUp: FC<{}> = () => {
         placeholder="이메일"
         password="email"
       />
+      <AuthMessage value="유효하지 않은 이메일입니다." reff={emailRef} />
       <AuthInput
         value={pw}
         handleChnage={handleChnage(setPw)}
         placeholder="비밀번호"
         password="password"
+      />
+      <AuthMessage
+        value="6자리 이상, 특수문자를 포함해야 합니다."
+        reff={pwRef}
       />
       <AuthInput
         value={pwCon}
@@ -36,11 +47,16 @@ const SignUp: FC<{}> = () => {
         placeholder="비밀번호 확인"
         password="password"
       />
+      <AuthMessage value="비밀번호가 일치하지 않습니다." reff={pwConRef} />
       <AuthInput
         value={username}
         handleChnage={handleChnage(SetUsername)}
         placeholder="이름"
         password="text"
+      />
+      <AuthMessage
+        value="사용할 수 없는 문자나 이름입니다."
+        reff={usernameRef}
       />
     </AuthBox>
   )
