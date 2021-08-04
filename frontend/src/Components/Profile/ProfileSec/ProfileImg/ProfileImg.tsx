78413eab8662a4163f1343f0175f6dde90f3d01c
5tarlight <developerstarlight@gmail.com>
2021-08-04 11:43:01 +0900

    upload picture

--- frontend/src/Components/Profile/ProfileSec/ProfileImg/ProfileImg.tsx
+++ frontend/src/Components/Profile/ProfileSec/ProfileImg/ProfileImg.tsx
@@ -1,7 +1,9 @@
-import { createRef, FC, memo } from 'react'
+import { ChangeEvent, createRef, FC, memo } from 'react'
 import styles from './ProfileImg.scss'
 import classNames from 'classnames/bind'
 import ProfileIcon from '../../../Header/ProfileDropdown/ProfileIcon/profile.png'
+import axios from 'axios'
+import { fileServer } from '../../../../config'
 
 const cx = classNames.bind(styles)
 
@@ -12,6 +14,38 @@ interface Props {
 const ProfileImg: FC<Props> = () => {
   const inputFile = createRef<HTMLInputElement>()
 
+  const handleChnage = async (event: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event
+
+    if (!files) {
+      // remove? or make other way to remove profile picture
+      return
+    }
+
+    const file = files[0]
+
+    const formData = new FormData()
+    console.log(files[0])
+    formData.append(
+      'profile_img',
+      file,
+      `${localStorage.getItem('id')}_profile.png`
+    )
+
+    const result = await axios.post(
+      `http://${fileServer}/profile/image`,
+      // { formData, id: localStorage.getItem('id') },
+      formData,
+      {
+        headers: { 'content-type': 'multipart/form-data' },
+      }
+    )
+
+    console.log(result)
+  }
+
   return (
     <div>
       <input
@@ -19,6 +53,7 @@ const ProfileImg: FC<Props> = () => {
         ref={inputFile}
         style={{ display: 'none' }}
         accept="image/*"
+        onChange={handleChnage}
       />
       <img
         className={cx('profile-img')}
