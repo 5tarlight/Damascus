1bb4d4d29226a9169b9909dbd0b133794b328064
5tarlight <developerstarlight@gmail.com>
2021-08-04 12:44:34 +0900

    disable x-powered-by header

--- file-server/index.js
+++ file-server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const multer = require('multer')
 
 const app = express()
+app.disable('x-powered-by')
 
 app.use(cors())
 app.use('/', express.static(`${__dirname}/files`))
