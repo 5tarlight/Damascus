9ca148bc3fb09e8462f89056bd768ab08169718e
5tarlight <developerstarlight@gmail.com>
2021-08-08 14:02:09 +0900

    logo remake

--- frontend/src/Components/Header/Logo/Logo.tsx
+++ frontend/src/Components/Header/Logo/Logo.tsx
@@ -1,4 +1,4 @@
-import { FC, MouseEvent as ME } from 'react'
+import { FC, memo, MouseEvent as ME } from 'react'
 import logo from './logo.png'
 import styles from './Logo.scss'
 import classNames from 'classnames/bind'
@@ -10,14 +10,16 @@ const Logo: FC<{}> = () => {
   const history = useHistory()
   const handleClick = (e: ME<HTMLDivElement, MouseEvent>) => {
     e.preventDefault()
+    e.stopPropagation()
     history.push('/')
   }
 
   return (
     <div className={cx('logo')} onClick={handleClick}>
-      <img src={logo} alt="DAMASCUS" className={cx('img-logo')} />
+      {/* <img src={logo} alt="DAMASCUS" className={cx('img-logo')} /> */}
+      DAMASCUS
     </div>
   )
 }
 
-export default Logo
+export default memo(Logo)
