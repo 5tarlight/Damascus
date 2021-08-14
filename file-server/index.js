e2ec57059b1d53a1aab96a16023043a827b98739
5tarlight <developerstarlight@gmail.com>
2021-08-14 11:49:30 +0900

    csrf

--- file-server/index.js
+++ file-server/index.js
@@ -2,23 +2,37 @@ const express = require('express')
 const cors = require('cors')
 const multer = require('multer')
 const helmet = require('helmet')
-// const scrf = require('csurf')
-// const sessions = require('express-session')
+// const csrf = require('csurf')
+// const session = require('express-session')
+// const cookieParser = require('cookie-parser')
+// const cookieSession = require('cookie-session')
+// const bodyParser = require('body-parser')
 
 // const scrfProtection = scrf({ cookie: false })
 const app = express()
 
+// app.use(cookieParser())
 // app.use(
-//   sessions({
-//     cookieName: 'demo-session',
-//     secret: 'this is a secret msg',
-//     duration: 30 * 60 * 1000,
-//     resave: false,
-//     saveUninitialized: false,
+//   session({ secret: 'keyboard cat', resave: false, saveUninitialized: true })
+// )
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(cookieParser())
+// app.use(
+//   cookieSession({
+//     name: 'session',
+//     keys: ['d', 'c'],
 //   })
 // )
 
-// const scrfProtection = scrf({ sessionKey: 'demo-session', cookie: false })
+// app.use(csrf({ cookie: true }))
+// app.use((req, res, next) => {
+//   const token = req.csrfToken()
+//   res.cookie('XSRF-TOKEN', token)
+//   res.locals.csrfToken = token
+//   next()
+// })
+
+// const scrfProtection = scrf({ sessionKey: 'session' })
 app.disable('x-powered-by')
 
 app.use(cors())
@@ -37,28 +51,22 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage }).single('profile_img')
 
-app.post(
-  '/profile/image',
-  /* scrfProtection , */ (req, res) => {
-    upload(req, res, err => {
-      if (err) {
-        return res.json({ success: false, err })
-      }
-      return res.json({
-        success: true,
-        image: res.req.file.path,
-        fileName: res.req.file.filename,
-      })
+app.post('/profile/image', (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      return res.json({ success: false, err })
+    }
+    return res.json({
+      success: true,
+      image: res.req.file.path,
+      fileName: res.req.file.filename,
     })
-  }
-)
+  })
+})
 
-app.get(
-  '/',
-  /* scrfProtection, */ (req, res) => {
-    res.end('damascus file server')
-  }
-)
+app.get('/', (req, res) => {
+  res.end('damascus file server')
+})
 
 app.listen(5677, () => {
   console.log('file server is on port 5677')
